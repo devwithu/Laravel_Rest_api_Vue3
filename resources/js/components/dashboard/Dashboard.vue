c016e13c171e8cbe97f29006ec21d02ea66142ba
devwithu <devwithu@naver.com>
2021-08-03 08:25:54 +0900

    dashboard page

--- resources/js/components/dashboard/Dashboard.vue
+++ resources/js/components/dashboard/Dashboard.vue
@@ -1,12 +1,24 @@
 <template>
-    <div>
-        This is coming from Dashboard.vue
-        {{ user }}
+    <div class="p-4 m-4 bg-white rounded flex">
+        <div>
+
+            <h1 class="text-2xl text-gray-700">Dashboard</h1>
+            <p class="mt-4">
+                Hello <span class="font-bold">{{ user.name }}</span>, Welcome to the dashboard, You have registerd with the email <span class="italic">{{ user.email }}</span>.
+            </p>
+            <p class="mt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti laboriosam libero
+                nesciunt nihil sed sit temporibus totam velit. Doloremque esse labore quod sequi veritatis. Facilis ipsa
+                ipsam ipsum magnam maxime!</p>
+            <p class="mt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti laboriosam libero
+                nesciunt nihil sed sit temporibus totam velit. Doloremque esse labore quod sequi veritatis. Facilis ipsa
+                ipsam ipsum magnam maxime!</p>
+        </div>
     </div>
 </template>
 
 <script>
 import {mapGetters} from 'vuex';
+
 export default {
     name: "Dashboard",
     computed: {
