32405e0139ce91773bc92ca5a08778c029a0c617
devwithu <devwithu@naver.com>
2021-08-03 08:53:22 +0900

    project list

--- resources/js/components/projects/Projects.vue
+++ resources/js/components/projects/Projects.vue
@@ -1,12 +1,44 @@
 <template>
-    <div>
-        This is coming from Projct.vue
+    <div class="p-4 m-4 bg-white rounded flex flex-col">
+        <div class="flex justify-between">
+            <h1 class="text-2xl text-gray-700">Projects</h1>
+            <button class="bg-gray-500 rounded text-white px-3 py-2 hover:bg-gray-700">Add Project</button>
+        </div>
+        <div class="justify-center flex">
+            <table class="table-auto justify-center">
+                <thead>
+                <tr>
+                    <th class="border px-4 py-2">ID</th>
+                    <th class="border px-4 py-2">Name</th>
+                    <th class="border px-4 py-2">Tasks</th>
+                    <th class="border px-4 py-2">Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                <ProjectItem v-for="project in projects" :key="project.id" :project="project"></ProjectItem>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
 <script>
+import ProjectItem from "./ProjectItem";
 export default {
-    name: "Projects"
+    components: {
+      ProjectItem
+    },
+    name: "Projects",
+    data() {
+        return {
+            projects: []
+        }
+    },
+    mounted() {
+        axios.get('api/projects').then( (res) => {
+            this.projects = res.data.data;
+        });
+    }
 }
 </script>
 
